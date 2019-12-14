02dc99a08fc25442b6bb7493a5aa616ff093909e
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-14 06:05:07 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -15,7 +15,10 @@ export default class Cell extends Component {
   }
   render(){
     return(
-      <div className = "cell" style={{backgroundColor: this.state.color}}  onClick={()=>this.changeColor('#333')}>{this.props.value}</div>
+      <div className = "cell" style={{backgroundColor: this.state.color}} 
+      onClick={()=>this.changeColor('#333')}>
+      {this.props.value}
+      </div>
       )
   }
 
